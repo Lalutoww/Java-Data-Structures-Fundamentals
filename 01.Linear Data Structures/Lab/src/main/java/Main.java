3453082cc1387676ae54543b70c5ec74fda77364
Martin Lalutov <martinlalutov00@gmail.com>
2025-03-15 16:49:59 +0200

    add array list impl

--- 01.Linear Data Structures/Lab/src/main/java/Main.java
+++ 01.Linear Data Structures/Lab/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import implementations.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> customArrayList = new ArrayList<>();
+        customArrayList.add(0);
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        customArrayList.add(4);
+        customArrayList.add(5);
+        System.out.println(customArrayList.size()); // 6
+
+        customArrayList.add(2, 5); // [0, 1, 5, 2, 3, 4, 5]
 
     }
 }
--- 01.Linear Data Structures/Lab/src/main/java/implementations/ArrayList.java
+++ 01.Linear Data Structures/Lab/src/main/java/implementations/ArrayList.java
@@ -2,57 +2,161 @@ package implementations;
 
 import interfaces.List;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayList<E> implements List<E> {
+    private static final int DEFAULT_CAPACITY = 4;
+    private int size;
+    private int capacity;
+    private Object[] arr;
+
+    public ArrayList() {
+        this.size = 0;
+        this.capacity = DEFAULT_CAPACITY;
+        arr = new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public boolean add(E element) {
-        return false;
+        if (this.size == this.capacity) {
+            this.arr = grow();
+        }
+
+        this.arr[size++] = element;
+        return true;
     }
 
     @Override
     public boolean add(int index, E element) {
-        return false;
+        validateIndex(index);
+        if (this.size == this.capacity) {
+            this.arr = grow();
+        }
+
+        if (index != this.size) {
+            shiftRight(index);
+        }
+
+        this.arr[index] = element;
+        this.size++;
+        return true;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public E get(int index) {
-        return null;
+        validateIndex(index);
+        return (E) this.arr[index];
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public E set(int index, E element) {
-        return null;
+        validateIndex(index);
+        E elementToReturn = (E) this.arr[index];
+
+        this.arr[index] = element;
+
+        return elementToReturn;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public E remove(int index) {
-        return null;
+        validateIndex(index);
+
+
+        E elementToReturn = (E) this.arr[index];
+
+        if (this.size < this.capacity / 3) {
+            this.arr = shrink();
+        }
+
+        if (index != this.size - 1) {
+            shiftLeft(index);
+        }
+
+        this.size--;
+        return elementToReturn;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.arr[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return indexOf(element) != -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public E next() {
+                if (arr[index] == (null) && hasNext()) {
+                    throw new NoSuchElementException("There is no such element in the collection");
+                }
+                return (E) arr[index++];
+            }
+        };
+    }
+
+    private Object[] grow() {
+        this.capacity *= 2;
+        return Arrays.copyOf(this.arr, this.capacity);
+    }
+
+    private Object[] shrink() {
+        this.capacity /= 2;
+        return Arrays.copyOf(this.arr, this.capacity);
+    }
+
+    private void shiftRight(int index) {
+        for (int i = this.size - 1; i >= index; i--) {
+            this.arr[i + 1] = this.arr[i];
+        }
+    }
+
+    private void shiftLeft(int index) {
+        for (int i = index; i < this.size; i++) {
+            this.arr[i] = this.arr[i + 1];
+        }
+    }
+
+    private boolean checkIndex(int index) {
+        return index < 0 || index >= this.size;
+    }
+
+    private void validateIndex(int index) {
+        if (checkIndex(index)) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds." + System.lineSeparator() + "Current size is: " + this.size);
+        }
     }
 }
